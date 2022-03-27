1e8391e4585694774346f66d0a42045eacc0f72a
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-27 19:25:40 +0500

    Implement Bible class

--- main.py
+++ main.py
@@ -13,6 +13,68 @@ class Book:
         return f"Book({self.book_number}, {self.short_name}, {self.long_name}, {self.book_color})"
 
 
+class Bible:
+    def __init__(self, **kwargs):
+        self.configuration = kwargs
+
+    def chapter_string(self):
+        return self.configuration.get("chapter_string", None)
+
+    def chapter_string_ot(self):
+        return self.configuration.get("chapter_string_ot", None)
+
+    def chapter_string_nt(self):
+        return self.configuration.get("chapter_string_nt", None)
+
+    def chapter_string_ps(self):
+        return self.configuration.get("chapter_string_ps", None)
+
+    def introduction_string(self):
+        return self.configuration.get("introduction_string", None)
+
+    def strong_numbers(self):
+        return self.configuration.get("strong_numbers", None)
+
+    def right_to_left(self):
+        return self.configuration.get("right_to_left", None)
+
+    def right_to_left_ot(self):
+        return self.configuration.get("right_to_left_ot", None)
+
+    def right_to_left_nt(self):
+        return self.configuration.get("right_to_left_nt", None)
+
+    def book_list_right_to_left(self):
+        return self.configuration.get("book_list_right_to_left", None)
+
+    def book_list_right_to_left_ot(self):
+        return self.configuration.get("book_list_right_to_left_ot", None)
+
+    def book_list_right_to_left_nt(self):
+        return self.configuration.get("book_list_right_to_left_nt", None)
+
+    def digits09(self):
+        return self.configuration.get("digits0-9", None)
+
+    def font_scale(self):
+        return self.configuration.get("font_scale", None)
+
+    def strong_numbers_prefix(self):
+        return self.configuration.get("strong_numbers_prefix", None)
+
+    def contains_accents(self):
+        return self.configuration.get("contains_accents", None)
+
+    def add_space_before_footnote_marker(self):
+        return self.configuration.get("add_space_before_footnote_marker", None)
+
+    def associated_theme(self):
+        return self.configuration.get("associated_theme", None)
+
+    def morphology_topic_reference(self, language=None):
+        return self.configuration.get("morphology_topic_reference" + ("_" + language) if language else "", None)
+
+
 def parse_books(filename) -> List[Book]:
     con = sqlite3.connect(filename)
     cur = con.cursor()
